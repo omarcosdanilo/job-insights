e38214b9a9a7184a0cf45cb2edbbfccb9721230f
Marcos Danilo <marcosdanilo94@gmail.com>
2022-10-21 14:43:45 -0300

    feat: :sparkles: 5  - implementa a função get_min_salary
    a função get_min_salary utiliza a função read para fazer leitura do arquivo csv e retorna o menor salário de acordo com a coluna min_salary do arquivo

--- src/insights.py
+++ src/insights.py
@@ -70,21 +70,13 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
+    jobs = read(path)
 
-    Must call `read`
+    salary_list = [
+        int(job['min_salary']) for job in jobs if (job['min_salary']).isdigit()
+    ]
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    return min(salary_list)
 
 
 def matches_salary_range(job, salary):
