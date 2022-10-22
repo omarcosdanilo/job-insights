06094a034decb52d0e6176e0efc4c3f34577efbf
Marcos Danilo <marcosdanilo94@gmail.com>
2022-10-22 17:25:29 -0300

    test: :white_check_mark: garante que a função read_brazilian_file retorna a lista de dicionários com as chaves em ingles

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,50 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
+
+data = [
+    {"title": "Maquinista", "salary": "2000", "type": "trainee"},
+    {"title": "Motorista", "salary": "3000", "type": "full time"},
+    {"title": "Analista de Software", "salary": "4000", "type": "full time"},
+    {
+        "title": "Assistente administrativo",
+        "salary": "1700",
+        "type": " full time",
+    },
+    {
+        "title": "Auxiliar administrativo",
+        "salary": "1400",
+        "type": " full time",
+    },
+    {"title": "Auxiliar usinagem", "salary": "1400", "type": " full time"},
+    {"title": "Auxiliar de padaria", "salary": "1400", "type": " full time"},
+    {"title": "Analista Contábil", "salary": "1400", "type": " full time"},
+    {"title": "Gerente comercial", "salary": "5000", "type": " full time"},
+    {
+        "title": "Analista de Departamento Pessoal",
+        "salary": "4000",
+        "type": " full time",
+    },
+    {
+        "title": "Esportista de Futebol profissional",
+        "salary": "50000",
+        "type": " full time",
+    },
+    {"title": "Analista de crédito", "salary": "4000", "type": " full time"},
+    {"title": "Pessoa Programadora", "salary": "3000", "type": " full time"},
+    {"title": "Ux Designer", "salary": "3000", "type": " full time"},
+    {
+        "title": "Auxiliar de manutenção",
+        "salary": " 1400",
+        "type": " full time",
+    },
+]
 
 
 def test_brazilian_jobs():
-    pass
+    translated_keys_list = read_brazilian_file(
+        "tests/mocks/brazilians_jobs.csv"
+    )
+
+    for obj in translated_keys_list:
+        assert 'title' in obj
+        assert 'salary' in obj
+        assert 'type' in obj
