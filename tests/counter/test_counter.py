5c185ff863a3d4f4135deff79401884b90b36ae2
Marcos Danilo <marcosdanilo94@gmail.com>
2022-10-22 16:27:15 -0300

    feat: :white_check_mark: 10 - implementa um teste para a função count_ocurrences
    garante que a função retornará o resultado esperado se os parâmetros são passados corretamente

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = (
+        '/home/marcos/Documentos/Projetos/' +
+        '4-Ciencia-da-Computacao/' +
+        '1-Job-Insights/sd-019-b-project-job-insights/src/jobs.csv'
+        )
+    assert count_ocurrences(path, 'TRAINEE') == 5
