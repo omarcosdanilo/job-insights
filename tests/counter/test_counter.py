6eca63f57c23064bb8917fb78939c4e6bde9d1f1
Marcos Danilo <marcosdanilo94@gmail.com>
2022-10-22 17:00:23 -0300

    refactor: :white_check_mark: refatora o teste da função count_occurences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,9 +2,10 @@ from src.counter import count_ocurrences
 
 
 def test_counter():
-    path = (
-        '/home/marcos/Documentos/Projetos/' +
-        '4-Ciencia-da-Computacao/' +
-        '1-Job-Insights/sd-019-b-project-job-insights/src/jobs.csv'
-        )
-    assert count_ocurrences(path, 'TRAINEE') == 5
+    path = ('src/jobs.csv')
+    assert count_ocurrences(path, 'Python') == 1639
+    assert count_ocurrences(path, 'PYTHON') == 1639
+    assert count_ocurrences(path, 'python') == 1639
+    assert count_ocurrences(path, 'Javascript') == 122
+    assert count_ocurrences(path, 'JAVASCRIPT') == 122
+    assert count_ocurrences(path, 'javascript') == 122
